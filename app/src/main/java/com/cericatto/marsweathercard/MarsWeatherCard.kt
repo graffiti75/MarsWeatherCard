cb42a4febbb89975fd85e2d748a5de63002147d0
Rodrigo Cericatto <graffiti75@gmail.com>
2025-03-31 21:40:50 -0300

    MarsWeatherCard height fixed

--- app/src/main/java/com/cericatto/marsweathercard/MarsWeatherCard.kt
+++ app/src/main/java/com/cericatto/marsweathercard/MarsWeatherCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -22,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -65,11 +67,13 @@ fun MarsWeatherCardRoot(
 fun MarsWeatherCard(
 	modifier: Modifier
 ) {
+	val configuration = LocalConfiguration.current
+	val screenHeight = (configuration.screenHeightDp * 0.4).dp
 	Column(
 		verticalArrangement = Arrangement.Center,
 		horizontalAlignment = Alignment.Start,
 		modifier = modifier
-			.padding(vertical = 250.dp)
+			.height(screenHeight)
 			.wrapContentWidth()
 			.background(MarsShapeBackground())
 			.padding(8.dp)
